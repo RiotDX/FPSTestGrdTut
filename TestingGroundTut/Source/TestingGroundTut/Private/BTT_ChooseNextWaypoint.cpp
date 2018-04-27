aab7713303fb93c4060476dbf3f785bd578012e1
RiotDX <riot.leader@gmail.com>
2018-04-27 15:43:19 +0100

    Waypoint following system

---
+++ TestingGroundTut/Source/TestingGroundTut/Private/BTT_ChooseNextWaypoint.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/BTT_ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Public/ThirdPersonGuard.h"
+#include "PatrolWaypoints.h"
+
+EBTNodeResult::Type UBTT_ChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	auto WaypointContainerComponent = OwnerComp.GetAIOwner()->GetPawn()->FindComponentByClass<UPatrolWaypoints>();
+	if(!ensure(WaypointContainerComponent)) { return EBTNodeResult::Failed; }
+
+	auto newWaypoint = WaypointContainerComponent->GetWaypoint(Index);
+	if (!ensure(newWaypoint)) { return EBTNodeResult::Failed; }
+
+	SetBlackboardWaypointValue(BlackboardComp, newWaypoint);
+
+	IncrementWaypointIndex(BlackboardComp, Index, WaypointContainerComponent);
+
+	return EBTNodeResult::Succeeded;
+}
+
+void UBTT_ChooseNextWaypoint::SetBlackboardWaypointValue(UBlackboardComponent* BlackboardComp, AActor* newWaypoint) {
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, newWaypoint);
+}
+
+void UBTT_ChooseNextWaypoint::IncrementWaypointIndex(UBlackboardComponent* BlackboardComp, int32 Index, UPatrolWaypoints* WaypointContainerComponent) {
+	auto newIndex = (Index + 1) % WaypointContainerComponent->GetWaypointCount();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, newIndex);
+}
+
+
