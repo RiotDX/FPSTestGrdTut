02a8070534051d2e81d7d8beab592a834d6f0308
RiotDX <riot.leader@gmail.com>
2018-05-02 16:59:46 +0200

    NPC Aiming

--- TestingGroundTut/Source/TestingGroundTut/TestingGroundTutGameMode.cpp
+++ TestingGroundTut/Source/TestingGroundTut/TestingGroundTutGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "TestingGroundTutGameMode.h"
 #include "TestingGroundTutHUD.h"
-#include "TestingGroundTutCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATestingGroundTutGameMode::ATestingGroundTutGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
