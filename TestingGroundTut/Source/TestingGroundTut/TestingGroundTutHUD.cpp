78d81762d24709191574523447091e05edb3cfcc
RiotDX <riot.leader@gmail.com>
2018-05-06 14:13:17 +0200

    Fixed strafe bug and merged TP and FP folders

--- TestingGroundTut/Source/TestingGroundTut/TestingGroundTutHUD.cpp
+++ TestingGroundTut/Source/TestingGroundTut/TestingGroundTutHUD.cpp
@@ -10,7 +10,7 @@
 ATestingGroundTutHUD::ATestingGroundTutHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Character/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
